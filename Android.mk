21c7a6546d24bedeab8f1491cf90e2c4c2862cf9
Kevin Jin <kjin@google.com>
2013-03-06 22:59:39 -0800

    add Android.mk for ActionBarSherlock See https://googleplex-android-review.googlesource.com/#/c/281133/ for usage

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, library/src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/library/res
+LOCAL_MANIFEST_FILE := library/AndroidManifest.xml
+
+LOCAL_JAVA_LIBRARIES := \
+  android-support-v4
+
+LOCAL_MODULE := ActionBarSherlock
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := 16
+include $(BUILD_STATIC_JAVA_LIBRARY)
